4b289f70296ffc8dee63e71e43b08f971732bc78
Ray Mak <projectraymak@gmail.com>
2024-04-03 21:00:35 -0700

    Implemented speech recognition

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,83 @@
 import React from "react"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faRightLong } from '@fortawesome/free-solid-svg-icons'
-import microphoneWhite from "../public/microphone-white.svg"
-import microphoneOrange from "../public/microphone-orange.svg"
-import { useState } from "react"
+import microphoneWhite from "/microphone-white.svg"
+import microphoneOrange from "/microphone-orange.svg"
+import { useState, useEffect, useRef } from "react"
 
 function App() {
   const [listening, setListening] = useState(false)
-  function toggleListening() {
-    setListening(prevState => !prevState)
+  const [transcript, setTranscript] = useState([])
+  const [error, setError] = useState(null)
+  const outputContainerRef = useRef(null)
+  const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition
+  const recognition = new SpeechRecognition()
+  recognition.continuous = false
+  recognition.interimResults = true
+  recognition.lang = "en-US"
+  recognition.onerror = (e) => {
+    console.error(e.error)
+    setError("Error occurred in recognition: " + e.error)
   }
+  
+  const englishText = transcript.map((text, index) => {
+    if (text !== "") {
+      return <div className="text-container" key={index}>
+      <p className="english-text">{text}</p>
+    </div>
+    }
+  })
+  
+  function toggleListening() {
+    if (listening) {
+      setTimeout(() => {
+        setListening(false); 
+      }, 1000); 
+    } else {
+      setListening(prevState => !prevState)
+    }
+  }
+
+  useEffect(() => {
+    let tempScript = []
+    function handleResult(e) {
+      if (e.results[0].isFinal === true) {
+        tempScript.push(e.results[0][0].transcript)
+        console.log(tempScript)
+      }
+    }
+    function handleEnd() {
+      console.log("recognition ended")
+      if (listening) {
+        recognition.start()
+      }
+    }
+
+    recognition.addEventListener('result', handleResult)
+    recognition.addEventListener('end', handleEnd)
+
+    return () => {
+      setTranscript(prevScript => [...prevScript, tempScript.join(", ")])
+      recognition.removeEventListener('result', handleResult)
+      recognition.removeEventListener('end', handleEnd)
+    }
+  }, [listening])
+
+  useEffect(() => {
+    if (listening) {
+      recognition.start()
+    } else {
+      recognition.stop()
+    }
+  }, [listening])
+
+  useEffect(() => {
+    if (outputContainerRef.current) {
+      outputContainerRef.current.scrollTop = outputContainerRef.current.scrollHeight
+    }
+  }, [transcript])
+
+  
 
   return (
     <div className="main-container">
@@ -25,30 +93,25 @@ function App() {
           <option value="arabic">Arabic</option>
         </select>
       </div>
-      <div className="output-container">
+      <div className="output-container" ref={outputContainerRef}>
         <div className="text-container">
           <p className="english-text">Hello, this is a test message</p>
           <div className="divider"></div>
           <p className="translated-text">您好，这是一条测试消息</p>
         </div>
-        <div className="text-container">
+        {/* <div className="text-container">
           <p className="english-text">Just writing some more random text to fill out the text container. This will be a slightly longer message. Nice!</p>
           <div className="divider"></div>
           <p className="translated-text">只需编写一些随机文本来填充文本容器即可。这将是一条稍长的消息。好的！</p>
         </div>
-        <div className="text-container">
-          <p className="english-text">Just writing some more random text to fill out the text container. This will be a slightly longer message. Nice!</p>
-          <div className="divider"></div>
-          <p className="translated-text">只需编写一些随机文本来填充文本容器即可。这将是一条稍长的消息。好的！</p>
-        </div>
-        <div className="text-container">
-          <p className="english-text">Hello, this is a test message</p>
-          <div className="divider"></div>
-          <p className="translated-text">您好，这是一条测试消息</p>
-        </div>
+         */}
+        {englishText}
+        {error && <p className="error">{error}</p>} 
       </div>
+      
       <div className="button-container">
-        {listening && <p className="listening">Listening...</p>}
+        {listening && <p className="listening">Listening...</p>}    
+           
         <button className="record-btn" type="button" aria-label="record audio" onClick={toggleListening}></button>
         {!listening && <img className="microphone" src={microphoneWhite} alt="white microphone icon"/>}
         {listening && <img className="microphone" src={microphoneOrange} alt="orange microphone icon"/>}
@@ -60,6 +123,7 @@ function App() {
           <span className="delay4"></span>
         </div>}
       </div>
+      
     </div>
   )
 }
